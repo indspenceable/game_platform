e4a69033fd1acb50264f7bdb1a61857d49d2fc55
Danny Spencer <dspencer@oberlin.edu>
2011-07-24 19:04:01 -0700

    Making challenges work succesfully. Not finished.

--- public/javascripts/lobby.js
+++ public/javascripts/lobby.js
@@ -1,17 +1,61 @@
+var players = [];
+var challenges = [];
+
+function remove(list,item){
+  for (var i = 0; i < list.length; i++ ) {
+    if (list[i] == item) {
+      return list.splice(i,1);
+    }
+  }
+  return list;
+}
+
+function removePlayer(pl_name) {
+  remove(players,pl_name);
+  $("#players").remove("#player_"+pl_name);
+}
+
+function addPlayer(pl_name) {
+  players.push(response['add_player'][i]);
+  $("#players").append($('<div/>', {
+    id:'player_'+pl_name
+    text:pl_name
+  }
+  //'<div id="player_' + pl_name'><b>' + pl_name + "</b></div>");
+}
+
+function addChallenge(pl_name) {
+  $("#player_"+pl_name).append($('<input/>', {
+    type:'button',
+    id:'challenge_'+pl_name,
+    value:'challenge_'+pl_name,
+  }));
+}
+function removeChallenge(pl_name) {
+  remove(challenges,pl_name);
+  $("#challenges").remove("#challenge_"+pl_name).
+}
+
 function update() {
-  $.get("/poll_lobby",function(response) {
+  $.get("/poll_lobby",players,function(response) {
     if ('redirect' in response) {
       window.location = '/' + response['redirect']
     } else {
-      var new_player_list = ""
-      for (var i =0; i < response['players'].length; i++) {
-        new_player_list += ('<br/>' + response['players'][i]);
+      for (var i =0; i < response['add_player'].length; i++) {
+        addPlayer(response['add_player'][i]
       }
-      $("#players:first").html(new_player_list);
+      for (var i =0; i < response['remove_player'].length; i++) {
+        removePlayer(response['remove_player'][i]);
+      }
+      for (var i = 0; i < response['challenge'].length; i++ ) {
+        addChallenge(response['challenge'][i])
+      }
+      for (var i = 0; i < response['unchallenge'].length; i++ ) {
+        removeChallenge(response['unchallenge'][i])
+      }
     }
   });
 }
-var players;
 
 $(document).ready(function() {
   //Join Game button
